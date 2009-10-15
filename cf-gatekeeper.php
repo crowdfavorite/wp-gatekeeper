a49e821bfd19edfda6ac8768056ab96f610a8b1c
mrichmond <mrichmond@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-10-15 14:57:27 +0000

    Making major additions to the gatekeeper plugin
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-gatekeeper/trunk@17186 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-gatekeeper.php
+++ cf-gatekeeper.php
@@ -4,7 +4,7 @@ Plugin Name: CF Gatekeeper
 Description: Redirect to login page if the user is not logged in.
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
-Version: 1.1(trunk)
+Version: 1.5(trunk)
 */
 
 define('CF_GATEKEEPER', true);
@@ -13,16 +13,26 @@ if (!defined('PLUGINDIR')) {
 	define('PLUGINDIR','wp-content/plugins');
 }
 if (is_file(trailingslashit(ABSPATH.PLUGINDIR).'cf-gatekeeper.php')) {
-	define('CFSZ_FILE', trailingslashit(ABSPATH.PLUGINDIR).'cf-gatekeeper.php');
+	define('CFGK_FILE', trailingslashit(ABSPATH.PLUGINDIR).'cf-gatekeeper.php');
 }
 else if (is_file(trailingslashit(ABSPATH.PLUGINDIR).'cf-gatekeeper/cf-gatekeeper.php')) {
-	define('CFSZ_FILE', trailingslashit(ABSPATH.PLUGINDIR).'cf-gatekeeper/cf-gatekeeper.php');
+	define('CFGK_FILE', trailingslashit(ABSPATH.PLUGINDIR).'cf-gatekeeper/cf-gatekeeper.php');
 }
 
 /* Do inital assignment of cf_user_key's */
-register_activation_hook(CFSZ_FILE, 'cfgk_process_users');
+register_activation_hook(CFGK_FILE, 'cfgk_process_users');
 
+/* Load localization library */
 load_plugin_textdomain('cf_gatekeeper');
+
+/* Define if we're enabled */
+if (get_option('cfgk_enabled')) {
+	define('CFGK_ENABLED', true);
+}
+else {
+	define('CFGK_ENABLED', false);
+}
+
 
 function cf_gatekeeper() {
 	global $userdata;
@@ -40,8 +50,10 @@ function cf_gatekeeper() {
 	}
 }
 
-// todo: Do a check of the privacy setting of the blog here
-add_action('init', 'cf_gatekeeper');
+/* See if we're enabled */
+if (CFGK_ENABLED) {
+	add_action('init', 'cf_gatekeeper');	
+}
 
 class cf_user_api {
 	function generate_key($user_id) {
@@ -113,6 +125,9 @@ function cfgk_process_users() {
 		$cf_user_api = new cf_user_api(); 
 	}
 	$cf_user_api->process_users();
+	
+	/* Don't turn on by default */
+	update_option('cfgk_enabled', '0');
 }
 
 function cfgk_add_key_to_user($user_id, $unused = null) {
@@ -120,10 +135,13 @@ function cfgk_add_key_to_user($user_id, $unused = null) {
 	$cf_user_api->add_key_to_user($user_id);
 }
 
-$cf_user_api = new cf_user_api();
+/* See if we're enabled */
+if (CFGK_ENABLED) {
+	$cf_user_api = new cf_user_api();
 
-add_action('user_register', 'cfgk_add_key_to_user');
-add_action('profile_update', 'cfgk_add_key_to_user');
+	add_action('user_register', 'cfgk_add_key_to_user');
+	add_action('profile_update', 'cfgk_add_key_to_user');
+}
 
 function cfgk_user_api_feeds($url) {
 	global $userdata;
@@ -140,13 +158,15 @@ function cfgk_user_api_feeds($url) {
 	}
 	return $url;
 }
-add_filter('feed_link', 'cfgk_user_api_feeds');
-add_filter('category_feed_link', 'cfgk_user_api_feeds');
-add_filter('tag_feed_link', 'cfgk_user_api_feeds');
-add_filter('search_feed_link', 'cfgk_user_api_feeds');
-add_filter('author_feed_link', 'cfgk_user_api_feeds');
-add_filter('post_comments_feed_link', 'cfgk_user_api_feeds');
-
+/* See if we're enabled */
+if (CFGK_ENABLED) {
+	add_filter('feed_link', 'cfgk_user_api_feeds');
+	add_filter('category_feed_link', 'cfgk_user_api_feeds');
+	add_filter('tag_feed_link', 'cfgk_user_api_feeds');
+	add_filter('search_feed_link', 'cfgk_user_api_feeds');
+	add_filter('author_feed_link', 'cfgk_user_api_feeds');
+	add_filter('post_comments_feed_link', 'cfgk_user_api_feeds');
+}
 function cfgk_show_api_key() {
 	global $profileuser;
 	$key = get_usermeta($profileuser->ID, 'cf_user_key');
@@ -159,7 +179,82 @@ function cfgk_show_api_key() {
 </table>
 <?php
 }
-add_action('show_user_profile', 'cfgk_show_api_key');
-add_action('edit_user_profile', 'cfgk_show_api_key');
+/* See if we're enabled */
+if (CFGK_ENABLED) {
+	add_action('show_user_profile', 'cfgk_show_api_key');
+	add_action('edit_user_profile', 'cfgk_show_api_key');
+}
 
+function cfgk_request_handler(){
+	if (isset($_POST['cf_action'])) {
+		switch ($_POST['cf_action']) {
+			case 'save_gatekeeper_options':
+				if (update_option('cfgk_enabled', $_POST['cfgk_enable_gatekeeper'])) {
+					if (get_option('cfgk_enabled')) {
+						do_action('cfgk_enabled');
+					}
+					else {
+						do_action('cfgk_disabled');
+					}
+				} 
+				if ($_GET['updated']) {
+					wp_redirect($_SERVER['REQUEST_URI']);
+				}
+				else {
+					wp_redirect($_SERVER['REQUEST_URI'].'&updated=true');
+				}
+				exit;
+			default:
+				break;
+		}
+	}
+}
+add_action('init', 'cfgk_request_handler');
+
+function cfgk_settings_form() {
+	$option_value = get_option('cfgk_enabled');
+
+	$enabled_options = array(
+		'Yes' => '1',
+		'No' => '0'
+	);
+	
+	$radio_inputs_html = '';
+	foreach ($enabled_options as $label => $value) {
+		if ($option_value == $value) {
+			$selected = ' checked="checked"';
+		}
+		else {
+			$selected = '';
+		}
+		$radio_inputs_html .= '
+			<input type="radio" name="cfgk_enable_gatekeeper" id="cfgk_enable_gatekeeper_'.attribute_escape(strtolower($label)).'" value="'.attribute_escape($value).'"'.$selected.' /> <label for="cfgk_enable_gatekeeper_'.attribute_escape(strtolower($label)).'">'.attribute_escape($label).'</label>
+		';
+	}
+	?>
+	<div class="wrap">
+		<h2>CF Gatekeeper</h2>
+		<form method="post">
+			<div>
+			<label for="cfgk_enable_gatekeeper">Enable Gatekeeper?</label>
+			<?php echo $radio_inputs_html; ?>
+			</div>
+			<button type="submit" class="button-primary">Save Option</button>
+			<input type="hidden" name="cf_action" value="save_gatekeeper_options" />
+		</form>
+	</div>
+	<?php
+}
+function cfgk_admin_menu() {
+	if (current_user_can('manage_options')) {
+		add_options_page(
+			__('CF Gatekeeper', '')
+			, __('CF Gatekeeper', '')
+			, 10
+			, basename(CFGK_FILE)
+			, 'cfgk_settings_form'
+		);
+	}
+}
+add_action('admin_menu', 'cfgk_admin_menu');
 ?>
